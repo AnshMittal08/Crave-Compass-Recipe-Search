b5d96a228e8fadd9fee8249afa85d8566e236878
AnshMittal08 <anshmittalstudy@gmail.com>
2024-09-19 18:18:50 +0530

    pushed the code of navbar

---
+++ index.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    var navList = document.querySelector('.nav-list');
+    var textOverlay = document.querySelector('.text-overlay');
+    var h1 = textOverlay.querySelector('h1');
+    var h3 = textOverlay.querySelectorAll('h3');
+
+    navList.classList.toggle('show');
+    if (navList.classList.contains('show')) {
+        h1.style.display = 'none';
+        h3.forEach(function (item) {
+            item.style.display = 'none';
+        });
+    } else {
+        h1.style.display = 'block';
+        h3.forEach(function (item) {
+            item.style.display = 'block';
+        });
+    }
+}
